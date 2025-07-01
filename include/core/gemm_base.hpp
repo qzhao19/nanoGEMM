32c2e908f5bb5c6a1bea7f513ff5d7c2072b3c32
qzhao19 <qi.zhao@outlook.fr>
2025-07-01 22:26:43 +0800

    update

--- include/core/gemm_base.hpp
+++ include/core/gemm_base.hpp
@@ -2,8 +2,25 @@
 #define GEMM_BASE_HPP_
 
 #include <cmath>
+#include <cstdint>
 #include <cstring>
 #include <new>
 
+namespace gemm {
+namespace detail {
 
+#define MEMORY_ALIGNMENT 32
+#define UNROLLING_SIZE 16
+
+template <typename T>
+T* malloc_aligned(std::int64_t m, std::int64_t n, std::int64_t size);
+
+template <typename T, std::int64_t MR, std::int64_t NR>
+void pack_matrix_A(std::int64_t m, std::int64_t k, T *A, std::int64_t lda, std::int64_t offset, T *packA);
+
+template <typename T, std::int64_t MR, std::int64_t NR>
+void pack_matrix_B(std::int64_t k, std::int64_t n, T *B, std::int64_t ldb, std::int64_t offset, T *packB);
+
+}
+}
 #endif // GEMM_BASE_HPP_

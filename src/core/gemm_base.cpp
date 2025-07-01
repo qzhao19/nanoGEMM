a828a248659a6275b6221faed8f349440f3e211a
qzhao19 <qi.zhao@outlook.fr>
2025-07-01 22:27:03 +0800

    create gemm_base.cpp

---
+++ src/core/gemm_base.cpp
@@ -0,0 +1,25 @@
+#include <core/gemm_base.hpp>
+
+namespace gemm {
+namespace detail {
+
+template <typename T>
+T* malloc_aligned(std::int64_t m, std::int64_t n, std::int64_t size) {
+    std::int64_t bytes = m * n * size;
+    if (bytes % MEMORY_ALIGNMENT != 0) {
+        bytes += MEMORY_ALIGNMENT - (bytes % MEMORY_ALIGNMENT);
+    }
+
+    void *ptr = std::aligned_alloc(MEMORY_ALIGNMENT, bytes);
+    if (!ptr) {
+        throw std::bad_alloc();
+    }
+    std::memset(ptr, 0, m * n * size);
+
+    return static_cast<T*>(ptr);
+}; 
+
+
+}
+}
+

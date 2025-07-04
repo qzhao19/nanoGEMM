babebc7bf7585a0afe8303829a284c70d8d544f0
qzhao19 <qi.zhao@outlook.fr>
2025-07-04 23:27:50 +0800

    update code

--- include/core/gemm_base.hpp
+++ include/core/gemm_base.hpp
@@ -13,13 +13,13 @@ namespace detail {
 #define UNROLLING_SIZE 16
 
 template <typename T>
-T* malloc_aligned(std::int64_t m, std::int64_t n, std::int64_t size);
+T* malloc_aligned(int64_t m, int64_t n, int64_t size);
 
-template <typename T, std::int64_t MR, std::int64_t NR>
-void pack_matrix_A(std::int64_t m, std::int64_t k, T *A, std::int64_t lda, std::int64_t offset, T *packA);
+template <typename T, int64_t MR, int64_t NR>
+void pack_matrix_A(int64_t m, int64_t k, T *A, int64_t lda, int64_t offset, T *packA);
 
-template <typename T, std::int64_t MR, std::int64_t NR>
-void pack_matrix_B(std::int64_t k, std::int64_t n, T *B, std::int64_t ldb, std::int64_t offset, T *packB);
+template <typename T, int64_t MR, int64_t NR>
+void pack_matrix_B(int64_t k, int64_t n, T *B, int64_t ldb, int64_t offset, T *packB);
 
 }
 }

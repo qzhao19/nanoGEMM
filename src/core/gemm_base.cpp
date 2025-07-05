5c21edfdb2f95bece9468ddecd961da2467010bc
qzhao19 <qi.zhao@outlook.fr>
2025-07-05 23:40:59 +0800

    update code

--- src/core/gemm_base.cpp
+++ src/core/gemm_base.cpp
@@ -19,6 +19,27 @@ T* malloc_aligned(int64_t m, int64_t n, int64_t size) {
     return static_cast<T*>(ptr);
 }; 
 
+template <typename T, int64_t MR, int64_t NR>
+void pack_matrix_A(int64_t m, int64_t k, T *A, int64_t lda, int64_t offset, T *packA) {
+    int64_t i, p;
+    T *a_ptr[MR];
+
+    for (i = 0; i < m; i++) {
+        a_ptr[i] = A + (offset + i);
+    }
+
+    for (i = m; i < MR; i++) {
+        a_ptr[ i ] = A + (offset + 0);
+    }
+
+    for (p = 0; p < k; p++) {
+        for (i = 0; i < MR; i++) {
+            *packA = *a_ptr[i];
+            packA ++;
+            a_ptr[i] = a_ptr[i] + lda;
+        }
+    }
+};
 
 }
 }

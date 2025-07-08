43b8010a0d0db7fccb32ada3d0b426c387535346
qzhao19 <qi.zhao@outlook.fr>
2025-07-08 23:54:28 +0800

    create gemm_8x4_kernel.hpp

---
+++ include/arch/x86/gemm_8x4_kernel.hpp
@@ -0,0 +1,17 @@
+#ifndef X86_GEMM_8X4_KERNEL_HPP_
+#define X86_GEMM_8X4_KERNEL_HPP_
+
+#include <gemm_kernels.hpp>
+
+namespace gemm {
+namespace detail {
+
+template <int64_t MR = 8, int64_t NR = 4>
+inline void AddDot_8x4_kernel(int64_t k, float *a, float *b, float *c, int64_t ldc);
+
+template <int64_t MR = 8, int64_t NR = 4>
+inline void AddDot_8x4_kernel(int64_t k, double *a, double *b, double *c, int64_t ldc);
+
+}
+}
+#endif // X86_GEMM_8X4_KERNEL_HPP_

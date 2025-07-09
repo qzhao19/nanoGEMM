37fa86df599a1eef0ae4acb2472cdebdf37c4b8a
qzhao19 <qi.zhao@outlook.fr>
2025-07-09 23:27:36 +0800

    update

--- include/gemm.hpp
+++ include/gemm.hpp
@@ -6,23 +6,24 @@
 namespace gemm {
 
 template <typename TA, typename TB, typename TC, 
-          int RM = 4, int RN = 1, 
-          int CM = 72, int CK = 256, int CN = 1020>
+          int64_t RM = 4, int64_t RN = 1, 
+          int64_t CM = 72, int64_t CK = 256, int64_t CN = 1020>
 class GEMM {
 private:
+    using MicroKernelType = std::function<void(int, TA*, TB*, TC*, int)>;
     const TA *const A_;
     const TB *const B_;
     TC *const C_;
 
-    const int lda_;
-    const int ldb_;
-    const int ldc_;
+    const int64_t lda_;
+    const int64_t ldb_;
+    const int64_t ldc_;
     MicroKernelType<TA, TB, TC, RM, RN> micro_kernel_;
 
 public:
-    GEMM(const TA *A, int lda, 
-         const TB *B, int ldb, 
-         TC *C, int ldc, 
+    GEMM(const TA *A, int64_t lda, 
+         const TB *B, int64_t ldb, 
+         TC *C, int64_t ldc, 
          MicroKernelType<TA, TB, TC, RM, RN> micro_kernel) :
             A_(A), lda_(lda), 
             B_(B), ldb_(ldb), 
@@ -30,7 +31,7 @@ public:
             micro_kernel_(micro_kernel) {};
     ~GEMM() = default;
     
-    void multiply(int m, int n, int k);
+    void multiply(int64_t m, int64_t n, int64_t k);
 }; 
 
 } 

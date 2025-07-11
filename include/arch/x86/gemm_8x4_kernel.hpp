89e09647ed79c7ca31ee321dc0a536485b06b88f
qzhao19 <qi.zhao@outlook.fr>
2025-07-11 22:45:41 +0800

    update

--- include/arch/x86/gemm_8x4_kernel.hpp
+++ include/arch/x86/gemm_8x4_kernel.hpp
@@ -6,10 +6,10 @@
 namespace gemm {
 namespace detail {
 
-template <int64_t MR = 8, int64_t NR = 4>
+template <int64_t RM = 8, int64_t RN = 4>
 inline void AddDot_8x4_kernel(int64_t k, float *a, float *b, float *c, int64_t ldc);
 
-template <int64_t MR = 8, int64_t NR = 4>
+template <int64_t RM = 8, int64_t RN = 4>
 inline void AddDot_8x4_kernel(int64_t k, double *a, double *b, double *c, int64_t ldc);
 
 }

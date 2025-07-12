b879a3d1d6fcbbc355cc19caeaec57bbb1e0d940
qzhao19 <qi.zhao@outlook.fr>
2025-07-12 23:40:40 +0800

    update

--- src/gemm.cpp
+++ src/gemm.cpp
@@ -5,12 +5,11 @@
 namespace gemm {
 
 template<typename TA, typename TB, typename TC, 
-          int64_t RM, int64_t RN, 
-          int64_t CM, int64_t CK, int64_t CN>    
-void GEMM<TA, TB, TC, RM, RN, CM, CK, CN>::multiply(int64_t m, int64_t n, int64_t k) {
+         int64_t CM, int64_t CK, int64_t CN,
+         int64_t RM, int64_t RN>    
+void GEMM<TA, TB, TC, CM, CK, CN, RM, RN>::multiply(int64_t m, int64_t n, int64_t k) {
     int64_t i, j, p;
     int64_t ic, jc, pc;
-    int64_t ib, jb, pb;
     int64_t min_m, min_k, min_n;
 
     TA *packA; 
@@ -19,35 +18,35 @@ void GEMM<TA, TB, TC, RM, RN, CM, CK, CN>::multiply(int64_t m, int64_t n, int64_
     packB = malloc_aligned<TB>(CK, CN + 1, sizeof(TB));
 
     for (jc = 0; jc < n; jc += CN) {
-        jb = std::min(n - jc, CN);
+        min_n = std::min(n - jc, CN);
 
         for (pc = 0; pc < k; pc += CK) {
-            pb = std::min(k - pc, CK);
+            min_k = std::min(k - pc, CK);
 
             // packing sub-matrix B 
-            for (j = 0; j < jb; j += RN) {
+            for (j = 0; j < min_n; j += RN) {
                 gemm::detail::pack_matrix_B<TB, RM, RN>(
-                    pb, 
-                    std::min(jb - j, RN),
+                    min_k, 
+                    std::min(min_n - j, RN),
                     &B_[pc],
                     k,
                     jc + j,
-                    &packB[j * pb]
+                    &packB[j * min_k]
                 );
             }
             
             for (ic = 0; ic < m; ic += CM) {
-                ib = std::min(m - ic, CM);
+                min_m = std::min(m - ic, CM);
                 
                 // packing sub-matrix A
-                for (i = 0; i < ib; i+= RM) {
+                for (i = 0; i < min_m; i+= RM) {
                     gemm::detail::pack_matrix_A<TB, RM, RN>(
-                        min(ib - i, RM), 
-                        pb, 
+                        min(min_m - i, RM), 
+                        min_k, 
                         &A[pc * lda], 
                         m, 
                         ic + i, 
-                        &packA[0 * CM * pb + i * pb]
+                        &packA[0 * CM * min_k + i * min_k]
                     );
                 }
 

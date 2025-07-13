ef5126ab1e9639b7a58bb3308288dabd8a1aba88
qzhao19 <qi.zhao@outlook.fr>
2025-07-13 23:43:54 +0800

    update

--- src/gemm.cpp
+++ src/gemm.cpp
@@ -4,18 +4,18 @@
 
 namespace gemm {
 
-template<typename TA, typename TB, typename TC, 
-         int64_t CM, int64_t CK, int64_t CN,
-         int64_t RM, int64_t RN>    
-void GEMM<TA, TB, TC, CM, CK, CN, RM, RN>::multiply(int64_t m, int64_t n, int64_t k) {
+template <typename TA, typename TB, typename TC, 
+          int64_t RM, int64_t RN,
+          int64_t CM, int64_t CK, int64_t CN>
+void GEMM<TA, TB, TC, RM, RN, CM, CK, CN>::multiply(int64_t m, int64_t n, int64_t k) {
     int64_t i, j, p;
     int64_t ic, jc, pc;
     int64_t min_m, min_k, min_n;
 
     TA *packA; 
     TB *packB;
-    packA = malloc_aligned<TA>(CK, CM + 1, sizeof(TA));
-    packB = malloc_aligned<TB>(CK, CN + 1, sizeof(TB));
+    packA = gemm::detail::malloc_aligned<TA>(CK, CM + 1, sizeof(TA));
+    packB = gemm::detail::malloc_aligned<TB>(CK, CN + 1, sizeof(TB));
 
     for (jc = 0; jc < n; jc += CN) {
         min_n = std::min(n - jc, CN);
@@ -41,9 +41,9 @@ void GEMM<TA, TB, TC, CM, CK, CN, RM, RN>::multiply(int64_t m, int64_t n, int64_
                 // packing sub-matrix A
                 for (i = 0; i < min_m; i+= RM) {
                     gemm::detail::pack_matrix_A<TB, RM, RN>(
-                        min(min_m - i, RM), 
+                        std::min(min_m - i, RM), 
                         min_k, 
-                        &A[pc * lda], 
+                        &A_[pc * lda_], 
                         m, 
                         ic + i, 
                         &packA[0 * CM * min_k + i * min_k]
@@ -56,9 +56,9 @@ void GEMM<TA, TB, TC, CM, CK, CN, RM, RN>::multiply(int64_t m, int64_t n, int64_
                             k,
                             &packA[i * k],
                             &packB[j * k],
-                            &C[j * ldc_ + i],
-                            ldc
-                        )
+                            &C_[j * ldc_ + i],
+                            ldc_
+                        );
                     }
                 }
             }
@@ -66,4 +66,7 @@ void GEMM<TA, TB, TC, CM, CK, CN, RM, RN>::multiply(int64_t m, int64_t n, int64_
     }
 };
 
-}
+template class gemm::GEMM<float, float, float, 4, 4, 72, 256, 2048>;
+template class gemm::GEMM<double, double, double, 4, 4, 72, 256, 2048>;
+
+} // gemm

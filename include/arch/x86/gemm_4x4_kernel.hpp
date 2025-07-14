e0a1a248bb867382153fa06347184da4d74ed0c7
qzhao19 <qi.zhao@outlook.fr>
2025-07-14 23:13:29 +0800

    update

--- include/arch/x86/gemm_4x4_kernel.hpp
+++ include/arch/x86/gemm_4x4_kernel.hpp
@@ -7,13 +7,13 @@ namespace gemm {
 namespace detail {
 
 template <int64_t RM = 4, int64_t RN = 4>
-inline void AddDot_4x4_kernel_float(int64_t k, float *a, float *b, float *c, int64_t ldc);
+void AddDot_4x4_kernel_float(int64_t k, float *a, float *b, float *c, int64_t ldc);
 
 template <int64_t RM = 4, int64_t RN = 4>
-inline void AddDot_4x4_kernel_double(int64_t k, double *a, double *b, double *c, int64_t ldc);
+void AddDot_4x4_kernel_double(int64_t k, double *a, double *b, double *c, int64_t ldc);
 
 template <typename TA, typename TB, typename TC, int64_t RM = 4, int64_t RN = 4>
-inline void AddDot_4x4_kernel(int64_t k, TA *a, TB *b, TC *c, int64_t ldc);
+void AddDot_4x4_kernel(int64_t k, TA *a, TB *b, TC *c, int64_t ldc);
 
 }
 }

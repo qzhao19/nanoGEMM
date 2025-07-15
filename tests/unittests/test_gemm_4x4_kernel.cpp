56ad293c2fb5ebc05044ea1a2da83c4fa606e27f
qzhao19 <qi.zhao@outlook.fr>
2025-07-15 23:46:02 +0800

    update

--- tests/unittests/test_gemm_4x4_kernel.cpp
+++ tests/unittests/test_gemm_4x4_kernel.cpp
@@ -5,23 +5,47 @@
 #include <limits>
 #include <cstdlib>
 #include <cmath>
-#include <include/gemm.hpp>
+#include <gemm.hpp>
 
 template<typename T>
-class GEMM8x4KernelTest : public ::testing::Test {
+class GEMM4x4KernelTest : public ::testing::Test {
 protected:
     void SetUp() override {
         engine_.seed(42);
         tol_ = 1E-10;
     }
 
-    T* generate_test_data(std::size_t size, T min = -1, T max = 1) {
+    T* generate_test_data(
+        std::size_t size, 
+        T min = -1, T max = 1) {
+        
         std::uniform_real_distribution<T> dist(min, max);
         T* arr = new T[size];
         for (std::size_t i = 0; i < size; ++i) {
             arr[i] = dist(engine_);
         }
         return arr;
+    }
+
+    void matmul_ref(
+        int m,
+        int n,
+        int k,
+        T *A,
+        int lda,
+        T *B,
+        int ldb,
+        T *C,
+        int ldc) {
+        
+        int    i, j, p;
+        for (i = 0; i < m; i ++) {
+            for (j = 0; j < n; j ++) {
+                for (p = 0; p < k; p ++) {
+                    C[j * ldc + i] += A[p * lda + i] * B[j * ldb + p];
+                }
+            }
+        }
     }
 
     bool compute_error(
@@ -62,28 +86,25 @@ protected:
 };
 
 using TestTypes = ::testing::Types<float, double>;
-TYPED_TEST_SUITE(GEMM8x4KernelTest, TestTypes);
+TYPED_TEST_SUITE(GEMM4x4KernelTest, TestTypes);
 
-TYPED_TEST(GEMM8x4KernelTest, BasicMultiply) {
+TYPED_TEST(GEMM4x4KernelTest, BasicMultiply) {
     using T = TypeParam;
-    constexpr int64_t M = 8, N = 4, K = 8;
+    int64_t M = 64, N = 64, K = 64;
+    int64_t lda = M, ldb = N, ldc = M;
     this->A = this->generate_test_data(M * K);
     this->B = this->generate_test_data(K * N);
     this->C = new T[M * N]();
     this->C_ref = new T[M * N]();
 
     // naive GEMM for reference
-    for (int64_t m = 0; m < M; ++m)
-        for (int64_t n = 0; n < N; ++n)
-            for (int64_t k = 0; k < K; ++k)
-                this->C_ref[m + n * M] += A[m + k * M] * B[k + n * K];
+    this->matmul_ref(M, N, K, this->A, lda, this->B, ldb, this->C_ref, ldc);
 
     // GEMM class
-    gemm::GEMM<T, T, T, 8, 4> gemm(
+    gemm::GEMM<T, T, T, 4, 4> gemm(
         this->A, M, this->B, K, this->C, M
     );
     gemm.multiply(M, N, K);
-
     bool errorFound = this->compute_error(M, M, M, N, this->C, this->C_ref);
     ASSERT_FALSE(errorFound) << "Errors found in the result.";
 

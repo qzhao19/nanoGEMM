37a7751a4ff2b24019cec681eed5f7a582a92f25
qzhao19 <qi.zhao@outlook.fr>
2025-07-17 23:32:44 +0800

    update

--- include/core/gemm_base.hpp
+++ include/core/gemm_base.hpp
@@ -14,7 +14,20 @@ namespace detail {
 #define UNROLLING_SIZE 16
 
 template <typename T>
-T* malloc_aligned(int64_t m, int64_t n, int64_t size);
+T* malloc_aligned(int64_t m, int64_t n, int64_t size) {
+    int64_t bytes = m * n * size;
+    if (bytes % MEMORY_ALIGNMENT != 0) {
+        bytes += MEMORY_ALIGNMENT - (bytes % MEMORY_ALIGNMENT);
+    }
+
+    void *ptr = std::aligned_alloc(MEMORY_ALIGNMENT, bytes);
+    if (!ptr) {
+        throw std::bad_alloc();
+    }
+    std::memset(ptr, 0, m * n * size);
+
+    return static_cast<T*>(ptr);
+}; 
 
 }
 }

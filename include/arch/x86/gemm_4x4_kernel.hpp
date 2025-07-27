72870e489e6556322779fc4ffd4d4d48572005d2
qzhao19 <qi.zhao@outlook.fr>
2025-07-27 22:53:27 +0800

    finalize AddDot_8x4_kernel_double function

--- include/arch/x86/gemm_4x4_kernel.hpp
+++ include/arch/x86/gemm_4x4_kernel.hpp
@@ -8,7 +8,7 @@ namespace detail {
 
 template <int64_t RM = 4, int64_t RN = 4>
 void AddDot_4x4_kernel_float(int64_t k, float *a, float *b, float *c, int64_t ldc) {
-    int i;
+    int64_t i;
     float alpha = 1.0f, beta = 1.0f;
     float *c0_3_0, *c0_3_1, *c0_3_2, *c0_3_3;
     
@@ -17,149 +17,191 @@ void AddDot_4x4_kernel_float(int64_t k, float *a, float *b, float *c, int64_t ld
     __m128 c0_3_0_xmm, c0_3_1_xmm, c0_3_2_xmm, c0_3_3_xmm;
 
     // accumulator
-    __m128 a0_3b_0_xmm, a0_3b_1_xmm, a0_3b_2_xmm, a0_3b_3_xmm;
-    a0_3b_0_xmm = _mm_setzero_ps();
-    a0_3b_1_xmm = _mm_setzero_ps();
-    a0_3b_2_xmm = _mm_setzero_ps();
-    a0_3b_3_xmm = _mm_setzero_ps();
+    __m128 a0_3b_0_xmm = setzeros<__m128>();
+    __m128 a0_3b_1_xmm = setzeros<__m128>();
+    __m128 a0_3b_2_xmm = setzeros<__m128>();
+    __m128 a0_3b_3_xmm = setzeros<__m128>();
 
     // pre-fetch a, b, and c data
-    __asm__ volatile( "prefetcht0 0(%0)          \n\t" : :"r"(a)  );
-	__asm__ volatile( "prefetcht2 0(%0)          \n\t" : :"r"(b)  );
-	__asm__ volatile( "prefetcht0 0(%0)          \n\t" : :"r"(c)  );
+    __asm__ volatile("prefetcht0 0(%0)          \n\t" : :"r"(a));
+    __asm__ volatile("prefetcht2 0(%0)          \n\t" : :"r"(b));
+    __asm__ volatile("prefetcht0 0(%0)          \n\t" : :"r"(c));
 
-    int64_t k_align = k / 4;
-    int64_t k_left = k % 4;
-    for (i = 0; i < k_align; ++i) {
+    // process blocks of 4 elements for better performance
+    int64_t k_blocks = k / 4;
+    int64_t k_remainder = k % 4;
+    
+    for (i = 0; i < k_blocks; ++i) {
         // compute 1st k value
-        a0_3_xmm = _mm_load_ps(a);
-        a0_3b_0_xmm = _mm_add_ps(a0_3b_0_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[0])));
-        a0_3b_1_xmm = _mm_add_ps(a0_3b_1_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[1])));
-        a0_3b_2_xmm = _mm_add_ps(a0_3b_2_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[2])));
-        a0_3b_3_xmm = _mm_add_ps(a0_3b_3_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[3])));
+        a0_3_xmm = load<__m128>(a);
+        a0_3b_0_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[0]), a0_3b_0_xmm);
+        a0_3b_1_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[1]), a0_3b_1_xmm);
+        a0_3b_2_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[2]), a0_3b_2_xmm);
+        a0_3b_3_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[3]), a0_3b_3_xmm);
 
-        // compute 2ed k value
-        a0_3_xmm = _mm_load_ps(a + 4);
-        a0_3b_0_xmm = _mm_add_ps(a0_3b_0_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[4])));
-        a0_3b_1_xmm = _mm_add_ps(a0_3b_1_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[5])));
-        a0_3b_2_xmm = _mm_add_ps(a0_3b_2_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[6])));
-        a0_3b_3_xmm = _mm_add_ps(a0_3b_3_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[7])));
+        // compute 2nd k value
+        a0_3_xmm = load<__m128>(a + 4);
+        a0_3b_0_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[4]), a0_3b_0_xmm);
+        a0_3b_1_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[5]), a0_3b_1_xmm);
+        a0_3b_2_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[6]), a0_3b_2_xmm);
+        a0_3b_3_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[7]), a0_3b_3_xmm);
 
-        __asm__ volatile( "prefetcht0 64(%0)          \n\t" : :"r"(a + 16)  );
+        if (i < k_blocks - 1) {
+            __asm__ volatile("prefetcht0 64(%0)          \n\t" : :"r"(a + 16));
+        }
+
         // compute 3rd k value
-        a0_3_xmm = _mm_load_ps(a + 8);
-        a0_3b_0_xmm = _mm_add_ps(a0_3b_0_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[8])));
-        a0_3b_1_xmm = _mm_add_ps(a0_3b_1_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[9])));
-        a0_3b_2_xmm = _mm_add_ps(a0_3b_2_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[10])));
-        a0_3b_3_xmm = _mm_add_ps(a0_3b_3_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[11])));
-
+        a0_3_xmm = load<__m128>(a + 8);
+        a0_3b_0_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[8]), a0_3b_0_xmm);
+        a0_3b_1_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[9]), a0_3b_1_xmm);
+        a0_3b_2_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[10]), a0_3b_2_xmm);
+        a0_3b_3_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[11]), a0_3b_3_xmm);
+        
         // compute 4th k value
-        a0_3_xmm = _mm_load_ps(a + 12);
-        a0_3b_0_xmm = _mm_add_ps(a0_3b_0_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[12])));
-        a0_3b_1_xmm = _mm_add_ps(a0_3b_1_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[13])));
-        a0_3b_2_xmm = _mm_add_ps(a0_3b_2_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[14])));
-        a0_3b_3_xmm = _mm_add_ps(a0_3b_3_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[15])));
-
+        a0_3_xmm = load<__m128>(a + 12);
+        a0_3b_0_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[12]), a0_3b_0_xmm);
+        a0_3b_1_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[13]), a0_3b_1_xmm);
+        a0_3b_2_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[14]), a0_3b_2_xmm);
+        a0_3b_3_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[15]), a0_3b_3_xmm);
+        
         // increment
         a += 16;
         b += 16;
-    } 
+    }
 
-    for (i = 0; i < k_left; ++i) {
-        a0_3_xmm = _mm_load_ps(a);
-        a0_3b_0_xmm = _mm_add_ps(a0_3b_0_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[0])));
-        a0_3b_1_xmm = _mm_add_ps(a0_3b_1_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[1])));
-        a0_3b_2_xmm = _mm_add_ps(a0_3b_2_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[2])));
-        a0_3b_3_xmm = _mm_add_ps(a0_3b_3_xmm, _mm_mul_ps(a0_3_xmm, _mm_set1_ps(b[3])));
+    // handle remaining elements
+    for (i = 0; i < k_remainder; ++i) {
+        a0_3_xmm = load<__m128>(a);
+        a0_3b_0_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[0]), a0_3b_0_xmm);
+        a0_3b_1_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[1]), a0_3b_1_xmm);
+        a0_3b_2_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[2]), a0_3b_2_xmm);
+        a0_3b_3_xmm = madd<__m128>(a0_3_xmm, set1<__m128>(b[3]), a0_3b_3_xmm);
         a += 4;
         b += 4;
     }
 
-    __m128 alpha_xmm = _mm_set1_ps(alpha);
-    __m128 beta_xmm = _mm_set1_ps(beta);
+    __m128 alpha_xmm = set1<__m128>(alpha);
+    __m128 beta_xmm = set1<__m128>(beta);
 
-    // 1st column
+    // Store results
     c0_3_0 = c + 0 * ldc;
-    c0_3_0_xmm = _mm_load_ps(c0_3_0);
-    c0_3_0_xmm = _mm_add_ps(_mm_mul_ps(beta_xmm, c0_3_0_xmm), _mm_mul_ps(alpha_xmm, a0_3b_0_xmm));
-    _mm_store_ps(c0_3_0, c0_3_0_xmm);
+    c0_3_0_xmm = load<__m128>(c0_3_0);
+    c0_3_0_xmm = add(mul(beta_xmm, c0_3_0_xmm), mul(alpha_xmm, a0_3b_0_xmm));
+    store(c0_3_0, c0_3_0_xmm);
 
-    // 2ed col
     c0_3_1 = c + 1 * ldc;
-    c0_3_1_xmm = _mm_load_ps(c0_3_1);
-    c0_3_1_xmm = _mm_add_ps(_mm_mul_ps(beta_xmm, c0_3_1_xmm), _mm_mul_ps(alpha_xmm, a0_3b_1_xmm));
-    _mm_store_ps(c0_3_1, c0_3_1_xmm);
+    c0_3_1_xmm = load<__m128>(c0_3_1);
+    c0_3_1_xmm = add(mul(beta_xmm, c0_3_1_xmm), mul(alpha_xmm, a0_3b_1_xmm));
+    store(c0_3_1, c0_3_1_xmm);
 
-    // 3rd col
     c0_3_2 = c + 2 * ldc;
-    c0_3_2_xmm = _mm_load_ps(c0_3_2);
-    c0_3_2_xmm = _mm_add_ps(_mm_mul_ps(beta_xmm, c0_3_2_xmm), _mm_mul_ps(alpha_xmm, a0_3b_2_xmm));
-    _mm_store_ps(c0_3_2, c0_3_2_xmm);
+    c0_3_2_xmm = load<__m128>(c0_3_2);
+    c0_3_2_xmm = add(mul(beta_xmm, c0_3_2_xmm), mul(alpha_xmm, a0_3b_2_xmm));
+    store(c0_3_2, c0_3_2_xmm);
 
-    // 4th col
     c0_3_3 = c + 3 * ldc;
-    c0_3_3_xmm = _mm_load_ps(c0_3_3);
-    c0_3_3_xmm = _mm_add_ps(_mm_mul_ps(beta_xmm, c0_3_3_xmm), _mm_mul_ps(alpha_xmm, a0_3b_3_xmm));
-    _mm_store_ps(c0_3_3, c0_3_3_xmm);
+    c0_3_3_xmm = load<__m128>(c0_3_3);
+    c0_3_3_xmm = add(mul(beta_xmm, c0_3_3_xmm), mul(alpha_xmm, a0_3b_3_xmm));
+    store(c0_3_3, c0_3_3_xmm);
 }
 
 
 template <int64_t RM = 4, int64_t RN = 4>
 void AddDot_4x4_kernel_double(int64_t k, double *a, double *b, double *c, int64_t ldc) {
-    double c_00_reg, c_01_reg, c_02_reg, c_03_reg;
-    double c_10_reg, c_11_reg, c_12_reg, c_13_reg;
-    double c_20_reg, c_21_reg, c_22_reg, c_23_reg;
-    double c_30_reg, c_31_reg, c_32_reg, c_33_reg;
+    int64_t i;
+    double alpha = 1.0, beta = 1.0;
+    double *c0_3_0, *c0_3_1, *c0_3_2, *c0_3_3;
 
-    c_00_reg = 0.0; c_01_reg = 0.0; c_02_reg = 0.0; c_03_reg = 0.0;
-    c_10_reg = 0.0; c_11_reg = 0.0; c_12_reg = 0.0; c_13_reg = 0.0;
-    c_20_reg = 0.0; c_21_reg = 0.0; c_22_reg = 0.0; c_23_reg = 0.0;
-    c_30_reg = 0.0; c_31_reg = 0.0; c_32_reg = 0.0; c_33_reg = 0.0;
+    // define matrix a0_3 and c0_3
+    __m256d a0_3_ymm;
+    __m256d c0_3_0_ymm, c0_3_1_ymm, c0_3_2_ymm, c0_3_3_ymm;
 
-    int p;
-    for (p = 0; p < k; p++) {
-        // pre-load A-row to register A(i, p) = A[j * RM + i]
-        double a_0p_reg = a[p * RM + 0];
-        double a_1p_reg = a[p * RM + 1];
-        double a_2p_reg = a[p * RM + 2];
-        double a_3p_reg = a[p * RM + 3];
+    // accumulator
+    __m256d a0_3b_0_ymm = setzeros<__m256d>();
+    __m256d a0_3b_1_ymm = setzeros<__m256d>();
+    __m256d a0_3b_2_ymm = setzeros<__m256d>();
+    __m256d a0_3b_3_ymm = setzeros<__m256d>();
 
-        // pre-load B-column to register B(p, j) = B[j * RN + i]
-        double b_p0_reg = b[p * RN + 0];
-        double b_p1_reg = b[p * RN + 1]; 
-        double b_p2_reg = b[p * RN + 2];
-        double b_p3_reg = b[p * RN + 3];
+    // pre-fetch a, b, and c data
+    __asm__ volatile("prefetcht0 0(%0)          \n\t" : :"r"(a));
+	__asm__ volatile("prefetcht2 0(%0)          \n\t" : :"r"(b));
+	__asm__ volatile("prefetcht0 0(%0)          \n\t" : :"r"(c));
 
-        // 1st row
-        c_00_reg += a_0p_reg * b_p0_reg;
-        c_01_reg += a_0p_reg * b_p1_reg;
-        c_02_reg += a_0p_reg * b_p2_reg;
-        c_03_reg += a_0p_reg * b_p3_reg;
+    int64_t k_blocks = k / 4;
+    int64_t k_remainder = k % 4;
 
-        // 2ed row
-        c_10_reg += a_1p_reg * b_p0_reg;
-        c_11_reg += a_1p_reg * b_p1_reg;
-        c_12_reg += a_1p_reg * b_p2_reg;
-        c_13_reg += a_1p_reg * b_p3_reg;
+    for (i = 0; i < k_blocks; ++i) {
+        // compute 1st k value
+        a0_3_ymm = load<__m256d>(a);
+        a0_3b_0_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[0]), a0_3b_0_ymm);
+        a0_3b_1_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[1]), a0_3b_1_ymm);
+        a0_3b_2_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[2]), a0_3b_2_ymm);
+        a0_3b_3_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[3]), a0_3b_3_ymm);
+
+        // compute 2ed k value
+        a0_3_ymm = load<__m256d>(a + 4);
+        a0_3b_0_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[4]), a0_3b_0_ymm);
+        a0_3b_1_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[5]), a0_3b_1_ymm);
+        a0_3b_2_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[6]), a0_3b_2_ymm);
+        a0_3b_3_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[7]), a0_3b_3_ymm);
+
+        __asm__ volatile("prefetcht0 64(%0)          \n\t" : :"r"(a + 16));
         
-        // 3rd row
-        c_20_reg += a_2p_reg * b_p0_reg;
-        c_21_reg += a_2p_reg * b_p1_reg;
-        c_22_reg += a_2p_reg * b_p2_reg;
-        c_23_reg += a_2p_reg * b_p3_reg;
-        
-        // 4th row
-        c_30_reg += a_3p_reg * b_p0_reg;
-        c_31_reg += a_3p_reg * b_p1_reg;
-        c_32_reg += a_3p_reg * b_p2_reg;
-        c_33_reg += a_3p_reg * b_p3_reg;
+        // compute 3rd k value
+        a0_3_ymm = load<__m256d>(a + 8);
+        a0_3b_0_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[8]), a0_3b_0_ymm);
+        a0_3b_1_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[9]), a0_3b_1_ymm);
+        a0_3b_2_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[10]), a0_3b_2_ymm);
+        a0_3b_3_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[11]), a0_3b_3_ymm);
+
+        // compute 4th k value
+        a0_3_ymm = load<__m256d>(a + 12);
+        a0_3b_0_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[12]), a0_3b_0_ymm);
+        a0_3b_1_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[13]), a0_3b_1_ymm);
+        a0_3b_2_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[14]), a0_3b_2_ymm);
+        a0_3b_3_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[15]), a0_3b_3_ymm);
+
+        // increment
+        a += 16;
+        b += 16;
     }
 
-    // C(i, j) = C[j * ldc + i]
-    c[0 * ldc + 0] += c_00_reg; c[1 * ldc + 0] += c_01_reg; c[2 * ldc + 0] += c_02_reg; c[3 * ldc + 0] += c_03_reg;
-    c[0 * ldc + 1] += c_10_reg; c[1 * ldc + 1] += c_11_reg; c[2 * ldc + 1] += c_12_reg; c[3 * ldc + 1] += c_13_reg;
-    c[0 * ldc + 2] += c_20_reg; c[1 * ldc + 2] += c_21_reg; c[2 * ldc + 2] += c_22_reg; c[3 * ldc + 2] += c_23_reg;
-    c[0 * ldc + 3] += c_30_reg; c[1 * ldc + 3] += c_31_reg; c[2 * ldc + 3] += c_32_reg; c[3 * ldc + 3] += c_33_reg;
+    for (i = 0; i < k_remainder; ++i) {
+        a0_3_ymm = load<__m256d>(a);
+        a0_3b_0_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[0]), a0_3b_0_ymm);
+        a0_3b_1_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[1]), a0_3b_1_ymm);
+        a0_3b_2_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[2]), a0_3b_2_ymm);
+        a0_3b_3_ymm = madd<__m256d>(a0_3_ymm, set1<__m256d>(b[3]), a0_3b_3_ymm);
+        a += 4;
+        b += 4;
+    }
+
+    __m256d alpha_ymm = set1<__m256d>(alpha);
+    __m256d beta_ymm = set1<__m256d>(beta);
+
+    // 1st column
+    c0_3_0 = c + 0 * ldc;
+    c0_3_0_ymm = load<__m256d>(c0_3_0);
+    c0_3_0_ymm = add(mul(beta_ymm, c0_3_0_ymm), mul(alpha_ymm, a0_3b_0_ymm));
+    store(c0_3_0, c0_3_0_ymm);
+
+    // 2ed col
+    c0_3_1 = c + 1 * ldc;
+    c0_3_1_ymm = load<__m256d>(c0_3_1);
+    c0_3_1_ymm = add(mul(beta_ymm, c0_3_1_ymm), mul(alpha_ymm, a0_3b_1_ymm));
+    store(c0_3_1, c0_3_1_ymm);
+
+    // 3rd col
+    c0_3_2 = c + 2 * ldc;
+    c0_3_2_ymm = load<__m256d>(c0_3_2);
+    c0_3_2_ymm = add(mul(beta_ymm, c0_3_2_ymm), mul(alpha_ymm, a0_3b_2_ymm));
+    store(c0_3_2, c0_3_2_ymm);
+
+    // 4th col
+    c0_3_3 = c + 3 * ldc;
+    c0_3_3_ymm = load<__m256d>(c0_3_3);
+    c0_3_3_ymm = add(mul(beta_ymm, c0_3_3_ymm), mul(alpha_ymm, a0_3b_3_ymm));
+    store(c0_3_3, c0_3_3_ymm);
 };
 
 template <typename TA, typename TB, typename TC, int64_t RM = 4, int64_t RN = 4>

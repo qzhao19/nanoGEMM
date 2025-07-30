0358f0eac6f3b1949b4077a2a9523350b4a2d82a
qzhao19 <qi.zhao@outlook.fr>
2025-07-30 23:48:27 +0800

    update

--- src/gemm.cpp
+++ src/gemm.cpp
@@ -2,74 +2,69 @@
 
 namespace gemm {
 
-// void matmul(int64_t m, int64_t n, int64_t k, 
-//            const float *A, int64_t lda,
-//            const float *B, int64_t ldb, 
-//            float *C, int64_t ldc) {
-//     // Select the optimal register block size and implementation based on matrix dimensions
-//     if (m % 16 == 0 && (m / 16 >= 6)) {
-//         // large-size, use 16x4 kernel
-//         gemm::detail::GEMM<float, float, float, 16, 4, 128, 256, 2048> 
-//             gemm_engine(A, lda, B, ldb, C, ldc);
-//         gemm_engine.multiply(m, n, k);
-//     }
-//     else if (m % 8 == 0) {
-//         // For medium-sized matrices, use an 8x4 kernel
-//         gemm::detail::GEMM<float, float, float, 8, 4, 96, 256, 1024> 
-//             gemm_engine(A, lda, B, ldb, C, ldc);
-//         gemm_engine.multiply(m, n, k);
-//     }
-//     else if (m % 4 == 0) {
-//         // For small matrices, use a 4x4 kernel
-//         gemm::detail::GEMM<float, float, float, 4, 4, 72, 256, 512> 
-//             gemm_engine(A, lda, B, ldb, C, ldc);
-//         gemm_engine.multiply(m, n, k);
-//     }
-//     else {
-//         // By default, use a 4x4 kernel
-//         gemm::detail::GEMM<float, float, float, 4, 4> 
-//             gemm_engine(A, lda, B, ldb, C, ldc);
-//         gemm_engine.multiply(m, n, k);
-//     }
-// };
-
-// void matmul(int64_t m, int64_t n, int64_t k,
-//            const double *A, int64_t lda,
-//            const double *B, int64_t ldb,
-//            double *C, int64_t ldc) {
-//     if (m % 16 == 0 && (m / 16 >= 6)) {
-//         gemm::detail::GEMM<double, double, double, 16, 4, 128, 256, 2048> 
-//             gemm_engine(A, lda, B, ldb, C, ldc);
-//         gemm_engine.multiply(m, n, k);
-//     }
-//     else if (m % 8 == 0) {
-//         gemm::detail::GEMM<double, double, double, 8, 4, 96, 256, 1024> 
-//             gemm_engine(A, lda, B, ldb, C, ldc);
-//         gemm_engine.multiply(m, n, k);
-//     }
-//     else {
-//         gemm::detail::GEMM<double, double, double, 4, 4> 
-//             gemm_engine(A, lda, B, ldb, C, ldc);
-//         gemm_engine.multiply(m, n, k);
-//     }
-// };
-
+template<int64_t RM, int64_t RN>
 void matmul(int64_t m, int64_t n, int64_t k, 
            const float *A, int64_t lda,
            const float *B, int64_t ldb, 
            float *C, int64_t ldc) {
     
-    gemm::detail::GEMM<float, float, float, 4, 4> gemm_engine(A, lda, B, ldb, C, ldc);
-    gemm_engine.multiply(m, n, k);
+    // gemm::detail::GEMM<float, float, float, 4, 4> gemm_engine(A, lda, B, ldb, C, ldc);
+    // gemm_engine.multiply(m, n, k);
+    if constexpr (RM == 4 && RN == 4) {
+        // 使用4x4内核
+        gemm::detail::GEMM<float, float, float, 4, 4> 
+            gemm_engine(A, lda, B, ldb, C, ldc, gemm::detail::AddDot_4x4_kernel_float<4, 4>);
+        gemm_engine.multiply(m, n, k);
+    }
+    else if constexpr (RM == 8 && RN == 4) {
+        // 使用8x4内核
+        gemm::detail::GEMM<float, float, float, 8, 4> 
+            gemm_engine(A, lda, B, ldb, C, ldc, gemm::detail::AddDot_8x4_kernel_float<8, 4>);
+        gemm_engine.multiply(m, n, k);
+    }
+    else {
+        // 默认使用4x4内核
+        gemm::detail::GEMM<float, float, float, 4, 4> 
+            gemm_engine(A, lda, B, ldb, C, ldc, gemm::detail::AddDot_4x4_kernel_float<4, 4>);
+        gemm_engine.multiply(m, n, k);
+    }
 }
 
+template<int64_t RM, int64_t RN>
 void matmul(int64_t m, int64_t n, int64_t k,
            const double *A, int64_t lda,
            const double *B, int64_t ldb,
            double *C, int64_t ldc) {
     
-    gemm::detail::GEMM<double, double, double, 4, 4> gemm_engine(A, lda, B, ldb, C, ldc);
-    gemm_engine.multiply(m, n, k);
+    if constexpr (RM == 4 && RN == 4) {
+        // 对于double类型，主要使用4x4内核
+        gemm::detail::GEMM<double, double, double, 4, 4> 
+            gemm_engine(A, lda, B, ldb, C, ldc, gemm::detail::AddDot_4x4_kernel_double<4, 4>);
+        gemm_engine.multiply(m, n, k);
+    }
+    else if constexpr (RM == 8 && RN == 4) {
+        // double类型的8x4内核（如果实现了的话）
+        gemm::detail::GEMM<double, double, double, 8, 4> 
+            gemm_engine(A, lda, B, ldb, C, ldc, gemm::detail::AddDot_8x4_kernel_double<8, 4>);
+        gemm_engine.multiply(m, n, k);
+    }
+    else {
+        // 默认使用4x4内核
+        gemm::detail::GEMM<double, double, double, 4, 4> 
+            gemm_engine(A, lda, B, ldb, C, ldc, gemm::detail::AddDot_4x4_kernel_double<4, 4>);
+        gemm_engine.multiply(m, n, k);
+    }
 }
+
+template void matmul<4, 4>(int64_t, int64_t, int64_t, 
+                          const float*, int64_t, const float*, int64_t, float*, int64_t);
+template void matmul<8, 4>(int64_t, int64_t, int64_t, 
+                          const float*, int64_t, const float*, int64_t, float*, int64_t);
+
+template void matmul<4, 4>(int64_t, int64_t, int64_t, 
+                          const double*, int64_t, const double*, int64_t, double*, int64_t);
+
+template void matmul<8, 4>(int64_t, int64_t, int64_t, 
+                          const double*, int64_t, const double*, int64_t, double*, int64_t);
 
 } // gemm

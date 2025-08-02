aebcfa73b33225224e81c6d58e29f15021a88c8a
qzhao19 <qi.zhao@outlook.fr>
2025-08-02 22:31:58 +0800

    update

--- src/gemm.cpp
+++ src/gemm.cpp
@@ -7,14 +7,14 @@ namespace detail {
 template<>
 void KernelFactory<float, float, float>::registerDefaultKernels() {
     // register kernel
-    registerKernel<4, 4>("4x4", AddDot_4x4_kernel_float<4, 4>);
-    registerKernel<8, 4>("8x4", AddDot_8x4_kernel_float<8, 4>);
+    registerKernel<4, 4, 72, 256, 2048>("4x4", AddDot_4x4_kernel<float, float, float, 4, 4>);
+    registerKernel<8, 4, 72, 256, 2048>("8x4", AddDot_8x4_kernel<float, float, float, 8, 4>);
 }
 
 template<>
 void KernelFactory<double, double, double>::registerDefaultKernels() {
-    registerKernel<4, 4>("4x4", AddDot_4x4_kernel_double<4, 4>);
-    registerKernel<8, 4>("8x4", AddDot_8x4_kernel_double<8, 4>);
+    registerKernel<4, 4, 72, 256, 2048>("4x4", AddDot_4x4_kernel<double, double, double, 4, 4>);
+    registerKernel<8, 4, 72, 256, 2048>("8x4", AddDot_8x4_kernel<double, double, double, 8, 4>);
 }
 
 }

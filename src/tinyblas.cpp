03e45483ae9b25d1a12433f1c647dce452fcfc13
qzhao19 <qi.zhao@outlook.fr>
2025-08-06 22:43:01 +0800

    update

--- src/tinyblas.cpp
+++ src/tinyblas.cpp
@@ -4,48 +4,54 @@
 
 namespace tinyBLAS {
 
-void matmul(int64_t m, int64_t n, int64_t k,
-            const float *A, int64_t lda,
-            const float *B, int64_t ldb,
-            float *C, int64_t ldc, 
+void matmul(int64_t m,
+            int64_t n,
+            int64_t k,
+            const float *A,
+            int64_t lda,
+            const float *B,
+            int64_t ldb,
+            float *C,
+            int64_t ldc,
             const std::string &kernel) {
-    
-    auto& factory = detail::KernelFactory<float, float, float>::get_instance();
-    
+    auto &factory = detail::KernelFactory<float, float, float>::get_instance();
+
     try {
         auto executor = factory.create_executor(kernel);
         executor->multiply(m, n, k, A, lda, B, ldb, C, ldc);
-    }
-    catch (const std::runtime_error& e) {
-        #ifdef DEBUG
+    } catch (const std::runtime_error &e) {
+#ifdef DEBUG
         throw std::invalid_argument("Kernel creation failed: " + std::string(e.what()));
-        #else
+#else
         auto executor = factory.create_executor("4x4");
         executor->multiply(m, n, k, A, lda, B, ldb, C, ldc);
-        #endif
+#endif
     }
 };
 
-void matmul(int64_t m, int64_t n, int64_t k,
-            const double *A, int64_t lda,
-            const double *B, int64_t ldb,
-            double *C, int64_t ldc,
+void matmul(int64_t m,
+            int64_t n,
+            int64_t k,
+            const double *A,
+            int64_t lda,
+            const double *B,
+            int64_t ldb,
+            double *C,
+            int64_t ldc,
             const std::string &kernel) {
-    
-    auto& factory = detail::KernelFactory<double, double, double>::get_instance();
-    
+    auto &factory = detail::KernelFactory<double, double, double>::get_instance();
+
     try {
         auto executor = factory.create_executor(kernel);
         executor->multiply(m, n, k, A, lda, B, ldb, C, ldc);
-    }
-    catch (const std::runtime_error& e) {
-        #ifdef DEBUG
+    } catch (const std::runtime_error &e) {
+#ifdef DEBUG
         throw std::invalid_argument("Kernel creation failed: " + std::string(e.what()));
-        #else
+#else
         auto executor = factory.create_executor("4x4");
         executor->multiply(m, n, k, A, lda, B, ldb, C, ldc);
-        #endif
+#endif
     }
 };
 
-} // namespace tinyBLAS
+}  // namespace tinyBLAS
--- src/tinyblas_gemm.cpp
+++ src/tinyblas_gemm.cpp
@@ -3,18 +3,40 @@
 namespace tinyBLAS {
 namespace detail {
 
-template<>
+template <>
 void KernelFactory<float, float, float>::register_default_kernels() {
     // register kernel
-    register_kernel<4, 4, 72, 256, 2048>("4x4", AddDot_4x4_kernel<float, float, float, 4, 4>);
-    register_kernel<8, 4, 72, 256, 2048>("8x4", AddDot_8x4_kernel<float, float, float, 8, 4>);
-};
+    register_kernel<4, 4, 72, 256, 2048>(
+        "4x4",
+        [](int64_t k, float* a, float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
+            AddDot_4x4_kernel<float, float, float, 4, 4>(k, a, b, c, ldc, ctx);
+        }
+    );
+    
+    register_kernel<8, 4, 72, 256, 2048>(
+        "8x4",
+        [](int64_t k, float* a, float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
+            AddDot_8x4_kernel<float, float, float, 8, 4>(k, a, b, c, ldc, ctx);
+        }
+    );
+}
 
-template<>
+template <>
 void KernelFactory<double, double, double>::register_default_kernels() {
-    register_kernel<4, 4, 72, 256, 2048>("4x4", AddDot_4x4_kernel<double, double, double, 4, 4>);
-    register_kernel<8, 4, 72, 256, 2048>("8x4", AddDot_8x4_kernel<double, double, double, 8, 4>);
-};
+    register_kernel<4, 4, 72, 256, 2048>(
+        "4x4",
+        [](int64_t k, double* a, double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
+            AddDot_4x4_kernel<double, double, double, 4, 4>(k, a, b, c, ldc, ctx);
+        }
+    );
+    
+    register_kernel<8, 4, 72, 256, 2048>(
+        "8x4",
+        [](int64_t k, double* a, double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
+            AddDot_8x4_kernel<double, double, double, 8, 4>(k, a, b, c, ldc, ctx);
+        }
+    );
+}
 
 }  // namespace detail
 }  // namespace tinyBLAS

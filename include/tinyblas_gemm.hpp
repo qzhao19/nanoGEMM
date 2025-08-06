14708d602ec5d6940b54c4f4f037cc655f09c815
qzhao19 <qi.zhao@outlook.fr>
2025-08-06 22:41:24 +0800

    update

--- include/tinyblas_gemm.hpp
+++ include/tinyblas_gemm.hpp
@@ -2,21 +2,26 @@
 #define TINYBLAS_GEMM_HPP_
 
 #include <algorithm>
-#include <string>
-#include <core/tinyblas_base.hpp>
-#include <core/kernel_factory.hpp>
 #include <arch/tinyblas_kernels.hpp>
 #include <arch/x86/tinyblas_gemm_4x4_kernel.hpp>
 #include <arch/x86/tinyblas_gemm_8x4_kernel.hpp>
+#include <core/kernel_factory.hpp>
+#include <core/tinyblas_base.hpp>
+#include <string>
 
 namespace tinyBLAS {
 namespace detail {
 
-template <typename TA, typename TB, typename TC, 
-          int64_t RM, int64_t RN,
-          int64_t CM, int64_t CK, int64_t CN>
+template <typename TA,
+          typename TB,
+          typename TC,
+          int64_t RM,
+          int64_t RN,
+          int64_t CM,
+          int64_t CK,
+          int64_t CN>
 class GEMM {
-private:
+   private:
     const TA *const A_;
     const TB *const B_;
     TC *const C_;
@@ -24,22 +29,18 @@ private:
     const int64_t lda_;
     const int64_t ldb_;
     const int64_t ldc_;
-    
+
     MicroKernelType<TA, TB, TC, RM, RN> micro_kernel_;
 
     void pack_matrix_A(
-        int64_t m, int64_t k, 
-        int64_t row_offset, int64_t col_offset, 
-        const TA *A, TA *packed_A) {  
-        
+        int64_t m, int64_t k, int64_t row_offset, int64_t col_offset, const TA *A, TA *packed_A) {
         int64_t i, p;
         const TA *a_ptr[RM];
 
         for (i = 0; i < RM; ++i) {
             if (i < m) {
                 a_ptr[i] = &A[(col_offset + 0) * lda_ + (row_offset + i)];
-            }
-            else {
+            } else {
                 a_ptr[i] = &A[(col_offset + 0) * lda_ + (row_offset + 0)];
             }
         }
@@ -50,15 +51,14 @@ private:
             for (i = 0; i < RM; ++i) {
                 if (i < m) {
                     // cache each row elements
-                    // move ptr to next col 
+                    // move ptr to next col
                     a_val[i] = *a_ptr[i];
                     a_ptr[i] += lda_;
-                }
-                else {
+                } else {
                     a_val[i] = 0;
                 }
             }
-            
+
             // then write to the pack buffer all at once
             for (i = 0; i < RM; ++i) {
                 *packed_A++ = a_val[i];
@@ -67,18 +67,14 @@ private:
     };
 
     void pack_matrix_B(
-        int64_t k, int64_t n, 
-        int64_t row_offset, int64_t col_offset, 
-        const TB *B, TB *packed_B) {
-        
+        int64_t k, int64_t n, int64_t row_offset, int64_t col_offset, const TB *B, TB *packed_B) {
         int64_t j, p;
         const TB *b_ptr[RN];
 
         for (j = 0; j < RN; ++j) {
             if (j < n) {
                 b_ptr[j] = &B[(col_offset + j) * ldb_ + row_offset];
-            }
-            else {
+            } else {
                 b_ptr[j] = &B[(col_offset + 0) * ldb_ + row_offset];
             }
         }
@@ -87,12 +83,11 @@ private:
             // read all values into a local array (in the cache)
             TB b_val[RN];
             for (j = 0; j < RN; j++) {
-                if (j < n) {  
+                if (j < n) {
                     // only access valid elements
                     b_val[j] = *b_ptr[j];
                     b_ptr[j]++;
-                }
-                else {  
+                } else {
                     // add proper zero padding
                     b_val[j] = 0;
                     // do not increment pointer for padding
@@ -105,19 +100,19 @@ private:
         }
     };
 
-public:
-    GEMM(const TA *A, int64_t lda, 
-         const TB *B, int64_t ldb, 
-         TC *C, int64_t ldc,
-         MicroKernelType<TA, TB, TC, RM, RN> micro_kernel) :
-            A_(A), lda_(lda), 
-            B_(B), ldb_(ldb), 
-            C_(C), ldc_(ldc),
-            micro_kernel_(micro_kernel) {};
+   public:
+    GEMM(const TA *A,
+         int64_t lda,
+         const TB *B,
+         int64_t ldb,
+         TC *C,
+         int64_t ldc,
+         MicroKernelType<TA, TB, TC, RM, RN> micro_kernel)
+        : A_(A), lda_(lda), B_(B), ldb_(ldb), C_(C), ldc_(ldc), micro_kernel_(micro_kernel){};
     ~GEMM() = default;
-    
+
     void multiply(int64_t m, int64_t n, int64_t k) {
-        // number of threads of inner dimension 
+        // number of threads of inner dimension
         int64_t ic_nts;
         int64_t i, j, p;
         int64_t ic, jc, pc;
@@ -131,9 +126,9 @@ public:
             ic_nts = std::strtol(str, nullptr, 10);
         }
 
-        TA *packA = malloc_aligned<TA>(CK, (CM + 1) * ic_nts, sizeof(TA)); 
+        TA *packA = malloc_aligned<TA>(CK, (CM + 1) * ic_nts, sizeof(TA));
         TB *packB = malloc_aligned<TB>(CK, CN + 1, sizeof(TB));
-        
+
         // 5-th loop around micro-kernel
         for (jc = 0; jc < n; jc += CN) {
             jb = std::min(n - jc, CN);
@@ -141,24 +136,22 @@ public:
             for (pc = 0; pc < k; pc += CK) {
                 pb = std::min(k - pc, CK);
 
-                // packing sub-matrix B 
+                // packing sub-matrix B
                 #pragma omp parallel for num_threads(ic_nts)
                 for (j = 0; j < jb; j += RN) {
-                    pack_matrix_B(
-                        pb,                      // number of rows to actually pack
-                        std::min(jb - j, RN),    // number of columns to actually pack
-                        pc,                      // global row offset
-                        jc + j,                  // global columns offset
-                        B_,                      // original matrix pointer
-                        &packB[j * pb]           // packed buffer
+                    pack_matrix_B(pb,                    // number of rows to actually pack
+                                  std::min(jb - j, RN),  // number of columns to actually pack
+                                  pc,                    // global row offset
+                                  jc + j,                // global columns offset
+                                  B_,                    // original matrix pointer
+                                  &packB[j * pb]         // packed buffer
                     );
                 }
-                
+
                 // start a parallel region
-                #pragma omp parallel num_threads(ic_nts) private(ic, ib, i) 
+                #pragma omp parallel num_threads(ic_nts) private(ic, ib, i)
                 {
                     int64_t thread_id = omp_get_thread_num();
-                    
                     // call partition_workload_by_thread
                     int64_t range_start, range_end;
                     partition_workload_by_thread(m, RM, range_start, range_end);
@@ -167,40 +160,47 @@ public:
                     for (ic = range_start; ic < range_end; ic += CM) {
                         ib = std::min(range_end - ic, CM);
                         // packing sub-matrix A
-                        for (i = 0; i < ib; i+= RM) {
+                        for (i = 0; i < ib; i += RM) {
                             pack_matrix_A(
-                                std::min(ib - i, RM),                   // number of rows to actually pack
-                                pb,                                     // umber of columns to actually pack
-                                ic + i,                                 // global row offset
-                                pc,                                     // global columns offset
-                                A_,                                     // original matrix pointer
-                                &packA[thread_id * CM * pb + i * pb]    // packed buffer position
+                                std::min(ib - i, RM),  // number of rows to actually pack
+                                pb,                    // umber of columns to actually pack
+                                ic + i,                // global row offset
+                                pc,                    // global columns offset
+                                A_,                    // original matrix pointer
+                                &packA[thread_id * CM * pb + i * pb]  // packed buffer position
                             );
                         }
-                        
+
+                        // define micro-kernel ctx
+                        MicroKernelCtxType<TB> ctx;
+                        ctx.next = packB;
+
                         // 2-th loop around micro-kernel
                         for (j = 0; j < jb; j += RN) {
+                            ctx.n = std::min(jb - j, RN);
                             for (i = 0; i < ib; i += RM) {
-                                micro_kernel_(
-                                    pb,
-                                    &packA[thread_id * CM * pb + i * pb],
-                                    &packB[j * pb],
-                                    &C_[(jc + j) * ldc_ + (ic + i)],
-                                    ldc_
-                                );
+                                ctx.m = std::min(ib - i, RM);
+                                if (i + RM > ib) {
+                                    ctx.next += pb * RN;
+                                }
+                                micro_kernel_(pb,
+                                              &packA[thread_id * CM * pb + i * pb],
+                                              &packB[j * pb],
+                                              &C_[(jc + j) * ldc_ + (ic + i)],
+                                              ldc_, 
+                                              &ctx);
                             }
                         }
                     }
                 }
-                
             }
         }
         free(packA);
         free(packB);
     };
-}; 
+};
 
-} // namespace detail
-} // namespace tinyBLAS
+}  // namespace detail
+}  // namespace tinyBLAS
 
-#endif // TINYBLAS_GEMM_HPP_
+#endif  // TINYBLAS_GEMM_HPP_

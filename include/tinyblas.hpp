0e19761d87fd2a60f196170e4a8e4414ad69c05d
qzhao19 <qi.zhao@outlook.fr>
2025-08-06 22:41:14 +0800

    update

--- include/tinyblas.hpp
+++ include/tinyblas.hpp
@@ -5,18 +5,28 @@
 
 namespace tinyBLAS {
 
-void matmul(int64_t m, int64_t n, int64_t k,
-            const float *A, int64_t lda,
-            const float *B, int64_t ldb,
-            float *C, int64_t ldc, 
+void matmul(int64_t m,
+            int64_t n,
+            int64_t k,
+            const float *A,
+            int64_t lda,
+            const float *B,
+            int64_t ldb,
+            float *C,
+            int64_t ldc,
             const std::string &kernel);
 
-void matmul(int64_t m, int64_t n, int64_t k,
-            const double *A, int64_t lda,
-            const double *B, int64_t ldb,
-            double *C, int64_t ldc,
+void matmul(int64_t m,
+            int64_t n,
+            int64_t k,
+            const double *A,
+            int64_t lda,
+            const double *B,
+            int64_t ldb,
+            double *C,
+            int64_t ldc,
             const std::string &kernel);
 
-}
+}  // namespace tinyBLAS
 
-#endif // namespace tinyBLAS
+#endif  // namespace tinyBLAS

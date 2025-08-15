e8ed2df8c426e1a1896888f6a09f02bfa5a684ea
qzhao19 <qi.zhao@outlook.fr>
2025-08-15 23:43:08 +0800

    update

--- src/tinyblas_gemv.cpp
+++ src/tinyblas_gemv.cpp
@@ -8,7 +8,10 @@ void matmul(int64_t m, int64_t n,
             float* y) {
 
     tinyBLAS::detail::GEMV<float, float, float, 32, 4, 72, 256> gemv_engine(
-        A, lda, x, y, &tinyBLAS::detail::AddDot_32x4_kernel_float<32, 4>);
+        A, lda, x, y, 
+        [](int64_t m, int64_t n, const float* A, int64_t lda, const float* x, float* y) {
+            tinyBLAS::detail::AddDot_32x4_kernel_float<32, 4>(m, n, A, lda, x, y);
+        });
     gemv_engine.multiply(m, n);
 };
 

2e90b3cbd7e1edcb0cfe50d63e3514248fc83fa3
qzhao19 <qi.zhao@outlook.fr>
2025-08-15 23:42:58 +0800

    update

--- include/tinyblas_gemv.hpp
+++ include/tinyblas_gemv.hpp
@@ -40,74 +40,64 @@ public:
         int64_t ic, ib, jc, jb;
         
         #pragma omp parallel for
-        for (jc = 0; jc < n; jc += CN) {
-            jb = std::min(jc + CN, n);
+        for (ic = 0; ic < m; ic += CM) {
+            ib = std::min(ic + CM, m);
 
-            for (ic = 0; ic < m; ic += CM) {
-                ib = std::min(ic + CM, m);
+            for (jc = 0; jc < n; jc += CN) {
+                jb = std::min(jc + CN, n);
 
                 for (int64_t i = ic; i < ib; i += RM) {
                     const int64_t nrows = std::min(ib - i, RM);
                     TY sum[RM] = {0.0f};
                     
+                    // define RM AVX vector registers to accumulate results
                     __m256 y_j_ymm[RM];
                     for (int64_t r = 0; r < nrows; ++r) {
                         y_j_ymm[r] = setzeros<__m256>();
                     }
-                    
+                    // handle one row data
                     for (int64_t j = jc; j + 7 < jb; j += 8) {
-                        __m256 x_j_ymm = load<__m256>(&x_[j]);
-                        
-                        for (int64_t r = 0; r < nrows; ++r) {
-                            float temp_a[8];
-                            for (int64_t k = 0; k < 8; ++k) {
-                                temp_a[k] = A_[(j+k) * lda_ + (i+r)];
-                            }
-                            
-                            __m256 a_rj_ymm = load<__m256>(temp_a);
+                        __m256 x_j_ymm = load<__m256, float>(&x_[j]);
+                        for (int r = 0; r < nrows; ++r) {
+                            __m256 a_rj_ymm = load<__m256, float>(&A_[(i + r) * lda_ + j]);
                             y_j_ymm[r] = madd<__m256>(a_rj_ymm, x_j_ymm, y_j_ymm[r]);
                         }
                     }
-                    
+                    // compute horizontal sum of each row
                     for (int64_t r = 0; r < nrows; ++r) {
                         sum[r] += hsum(y_j_ymm[r]);
                     }
 
+                    // handle rest elements
                     __m128 y_j_xmm[RM];
                     for (int64_t r = 0; r < nrows; ++r) {
                         y_j_xmm[r] = setzeros<__m128>();
                     }
-                    
+
+                    // find the remaining starting position after AVX2 processing
                     int64_t j = jc;
                     while (j + 7 < jb) j += 8;
-                    
-                    for (; j + 3 < jb; j += 4) {
-                        __m128 x_j_xmm = load<__m128>(&x_[j]);
-                        
+                    for (; j < jb; j += 4) {
+                        __m128 x_j_xmm = load<__m128, float>(&x_[j]);
                         for (int64_t r = 0; r < nrows; ++r) {
-                            float temp_a[4];
-                            for (int64_t k = 0; k < 4; ++k) {
-                                temp_a[k] = A_[(j+k) * lda_ + (i+r)];
-                            }
-                            
-                            __m128 a_rj_xmm = load<__m128>(temp_a);
+                            __m128 a_rj_xmm = load<__m128, float>(&A_[(i + r) * lda_ + j]);
                             y_j_xmm[r] = madd<__m128>(a_rj_xmm, x_j_xmm, y_j_xmm[r]);
                         }
                     }
                     
+                    // compute horizontal sum of each row
                     for (int64_t r = 0; r < nrows; ++r) {
                         sum[r] += hsum(y_j_xmm[r]);
                     }
 
+                    // handle last remaining
                     while (j + 7 < jb) j += 8;
                     while (j + 3 < jb) j += 4;
-                    
                     for (; j < jb; ++j) {
                         for (int64_t r = 0; r < nrows; ++r) {
-                            sum[r] += A_[j * lda_ + (i + r)] * x_[j];
+                            sum[r] += A_[(i + r) * lda_ + j] * x_[j];
                         }
                     }
-                    
                     for (int64_t r = 0; r < nrows; ++r) {
                         y_[i + r] += sum[r];
                     }

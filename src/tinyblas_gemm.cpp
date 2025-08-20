02fe7353205111f270abc83f8de585ade2f0be59
qzhao19 <qi.zhao@outlook.fr>
2025-08-20 23:47:16 +0800

    update

--- src/tinyblas_gemm.cpp
+++ src/tinyblas_gemm.cpp
@@ -14,9 +14,9 @@ void KernelFactory<float, float, float>::register_default_kernels() {
     );
     
     register_kernel<8, 4, 72, 256, 2048>(
-        "8x4",
+        "8x8",
         [](int64_t k, float* a, float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
-            AddDot_8x4_kernel<float, float, float, 8, 4>(k, a, b, c, ldc, ctx);
+            AddDot_8x8_kernel<float, float, float, 8, 8>(k, a, b, c, ldc, ctx);
         }
     );
 }
@@ -31,9 +31,9 @@ void KernelFactory<double, double, double>::register_default_kernels() {
     );
     
     register_kernel<8, 4, 72, 256, 2048>(
-        "8x4",
+        "8x8",
         [](int64_t k, double* a, double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
-            AddDot_8x4_kernel<double, double, double, 8, 4>(k, a, b, c, ldc, ctx);
+            AddDot_8x8_kernel<double, double, double, 8, 8>(k, a, b, c, ldc, ctx);
         }
     );
 }

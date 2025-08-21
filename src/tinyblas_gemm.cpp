a79a5492bb8694d73337715d66f02a801cd26aac
qzhao19 <qi.zhao@outlook.fr>
2025-08-21 23:32:08 +0800

    fix bug

--- src/tinyblas_gemm.cpp
+++ src/tinyblas_gemm.cpp
@@ -13,7 +13,7 @@ void KernelFactory<float, float, float>::register_default_kernels() {
         }
     );
     
-    register_kernel<8, 4, 72, 256, 2048>(
+    register_kernel<8, 8, 72, 256, 2048>(
         "8x8",
         [](int64_t k, float* a, float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
             AddDot_8x8_kernel<float, float, float, 8, 8>(k, a, b, c, ldc, ctx);
@@ -30,7 +30,7 @@ void KernelFactory<double, double, double>::register_default_kernels() {
         }
     );
     
-    register_kernel<8, 4, 72, 256, 2048>(
+    register_kernel<8, 8, 72, 256, 2048>(
         "8x8",
         [](int64_t k, double* a, double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
             AddDot_8x8_kernel<double, double, double, 8, 8>(k, a, b, c, ldc, ctx);

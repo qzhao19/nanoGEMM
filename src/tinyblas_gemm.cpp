0d9a0119769a7bfda8dda8fe2a982999ea52e4f4
qzhao19 <qi.zhao@outlook.fr>
2025-08-27 23:13:54 +0800

    code update

--- src/tinyblas_gemm.cpp
+++ src/tinyblas_gemm.cpp
@@ -8,14 +8,14 @@ void KernelFactory<float, float, float>::register_default_kernels() {
     // register kernel
     register_kernel<4, 4, 72, 256, 2048>(
         "4x4",
-        [](int64_t k, float* a, float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
+        [](int64_t k, const float* a, const float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
             AddDot_4x4_kernel<float, float, float, 4, 4>(k, a, b, c, ldc, ctx);
         }
     );
     
     register_kernel<8, 8, 72, 256, 2048>(
         "8x8",
-        [](int64_t k, float* a, float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
+        [](int64_t k, const float* a, const float* b, float* c, int64_t ldc, MicroKernelCtxType<float>* ctx) {
             AddDot_8x8_kernel<float, float, float, 8, 8>(k, a, b, c, ldc, ctx);
         }
     );
@@ -25,14 +25,14 @@ template <>
 void KernelFactory<double, double, double>::register_default_kernels() {
     register_kernel<4, 4, 72, 256, 2048>(
         "4x4",
-        [](int64_t k, double* a, double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
+        [](int64_t k, const double* a, const double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
             AddDot_4x4_kernel<double, double, double, 4, 4>(k, a, b, c, ldc, ctx);
         }
     );
     
     register_kernel<8, 8, 72, 256, 2048>(
         "8x8",
-        [](int64_t k, double* a, double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
+        [](int64_t k, const double* a, const double* b, double* c, int64_t ldc, MicroKernelCtxType<double>* ctx) {
             AddDot_8x8_kernel<double, double, double, 8, 8>(k, a, b, c, ldc, ctx);
         }
     );
